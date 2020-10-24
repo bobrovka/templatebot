87728f8bceba930123f4fb298ac8af2ca0e907ba
Kirill Bobrov <bobrov.it@yandex.ru>
2020-10-24 13:42:46 +0400

    bot init

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/bobrovka/templatebot/config"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
+func main() {
+	port := os.Getenv("port")
+
+	cfg := config.Config{
+		TgToken: "1375298760:AAGUhlCpoiEp5PXcN_b38nrWN40-r0jDKQ0",
+		Webhook: "https://somedifferenttestbot.herokuapp.com",
+	}
+
+	go func() {
+		_ = http.ListenAndServe(":"+port, nil)
+	}()
+
+	bot, err := tgbotapi.NewBotAPI(cfg.TgToken)
+	if err != nil {
+		log.Fatal("cannot new bot ", err)
+	}
+	log.Println("Bot created")
+
+	_, err = bot.SetWebhook(tgbotapi.NewWebhook(cfg.Webhook))
+	if err != nil {
+		log.Fatal("cannot new bot ", err)
+	}
+	log.Println("Webhook created")
+
+	for update := range bot.ListenForWebhook("/") {
+		if _, err := bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "A: "+update.Message.Text)); err != nil {
+			log.Println(err)
+		}
+	}
+	_ = port
+}
